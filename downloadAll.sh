5c51ca11294015af28d58680f5ea125f1fb6d93d
Antoine Dahan <adahan93@gmail.com>
2014-12-19 09:47:22 -0500

    bash file for pulling from 178.62.*.* ips

---
+++ downloadAll.sh
@@ -0,0 +1,17 @@
+download_from_ips() {
+    for THIRD_QUADRANT in $(seq 1 256)
+    do
+        for FOURTH_QUADRANT in $(seq 1 256)
+        do
+            IP_ADDRESS="178.62.${THIRD_QUADRANT}.${FOURTH_QUADRANT}"           $
+            if wget --spider --quiet --connect-timeout=5 --tries=2 $IP_ADDRESS
+            then
+                wget --quiet -O $IP_ADDRESS --connect-timeout=5 --tries=2 $IP_A$
+                sleep 4
+                kill $!
+            fi
+        done
+    done
+}
+
+download_from_ips
